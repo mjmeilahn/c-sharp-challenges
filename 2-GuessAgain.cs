65fb274c06df3d4971991bb341d4e0d1a7464118
mjmeilahn <sysadmin@SysAdmins-MacBook-Pro.local>
2018-05-09 13:07:41 -0700

    Adding GuessAgain game

---
+++ 2-GuessAgain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GuessAgain
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // console app that generates a random number
+            // user has limited attempts to guess the correct number
+
+            var random = new Random();
+            var randomOutput = random.Next(1, 5);
+            var tries = 0;
+
+            Console.WriteLine("random number is: " + randomOutput);
+
+            while (tries < 4)
+            {
+                var input = Convert.ToInt32(Console.ReadLine());
+
+                if (input != randomOutput) 
+                {
+                    tries++;
+                    if (tries == 4) 
+                    {
+                        Console.WriteLine("You Lose!");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Guess Again!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Correct!");
+                    break;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Keeping a public record of accomplished challenges with the C# language
 
 1. Bad Cop - watches the "speed limit", and fines users 5 demerits per digit
+
+2. Guess Again - picks a random number from 1-4, user has limited attempts to guess the correct number
